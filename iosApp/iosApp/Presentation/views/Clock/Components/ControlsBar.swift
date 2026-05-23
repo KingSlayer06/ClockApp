3150d077aa8b136b5ac2080c4c0a918f7b7eb347
KingSlayer06 <sherkarhimanshu00@gmail.com>
2026-05-23 12:16:37 +0200

    Controls Button UI fix for iOS

--- iosApp/iosApp/Presentation/views/Clock/Components/ControlsBar.swift
+++ iosApp/iosApp/Presentation/views/Clock/Components/ControlsBar.swift
@@ -64,7 +64,7 @@ private struct ControlButton: View {
                 .padding(.vertical, 5)
                 .overlay(
                     RoundedRectangle(cornerRadius: 6)
-                        .stroke(accent.opacity(0.5), lineWidth: 0.5)
+                        .stroke(accent, lineWidth: 1)
                 )
         }
     }
